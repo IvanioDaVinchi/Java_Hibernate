f6403aea1f8a20ac3a771e593a983f10f216e80b
losev <sloaev@gmail.com>
2021-05-18 11:24:39 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,38 +9,50 @@ import javax.persistence.metamodel.EntityType;
 
 import java.util.Map;
 
-public class Main {
+public class Main
+{
     private static final SessionFactory ourSessionFactory;
 
-    static {
-        try {
+    static
+    {
+        try
+        {
             Configuration configuration = new Configuration();
             configuration.configure();
 
             ourSessionFactory = configuration.buildSessionFactory();
-        } catch (Throwable ex) {
+        }
+        catch (Throwable ex)
+        {
             throw new ExceptionInInitializerError(ex);
         }
     }
 
-    public static Session getSession() throws HibernateException {
+    public static Session getSession() throws HibernateException
+    {
         return ourSessionFactory.openSession();
     }
 
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception
+    {
         final Session session = getSession();
-        try {
+        try
+        {
             System.out.println("querying all the managed entities...");
             final Metamodel metamodel = session.getSessionFactory().getMetamodel();
-            for (EntityType<?> entityType : metamodel.getEntities()) {
+            for (EntityType<?> entityType : metamodel.getEntities())
+            {
                 final String entityName = entityType.getName();
                 final Query query = session.createQuery("from " + entityName);
                 System.out.println("executing: " + query.getQueryString());
-                for (Object o : query.list()) {
+                for (Object o : query.list())
+                {
                     System.out.println("  " + o);
                 }
             }
-        } finally {
+        }
+        finally
+        {
             session.close();
         }
     }
