e0ef1349997d4326056ffa5bffd98886bf340d24
losev <sloaev@gmail.com>
2021-05-18 10:22:42 +0300

    commit

--- src/main/java/com/company/CarSupplierEntity.java
+++ src/main/java/com/company/CarSupplierEntity.java
@@ -1,14 +1,13 @@
 package com.company;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "car_supplier", schema = "cardealership")
 public class CarSupplierEntity {
     private int id;
+    private CarsEntity carsByIdCar;
+    private SuppliersEntity supplierByIdSupplier;
 
     @Id
     @Column(name = "ID", nullable = false)
@@ -19,6 +18,22 @@ public class CarSupplierEntity {
     public void setId(int id) {
         this.id = id;
     }
+
+    @ManyToOne
+    @JoinColumn(name = "IDCar", referencedColumnName = "ID")
+    public CarsEntity getCarsByIdCar() {
+        return carsByIdCar;
+    }
+
+    public void setCarsByIdCar(CarsEntity carsByIdCar) {
+        this.carsByIdCar = carsByIdCar;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "IDSupplier", referencedColumnName = "ID")
+    public SuppliersEntity getSupplierByIdSupplier() { return supplierByIdSupplier; }
+
+    public void setSupplierByIdSupplier(SuppliersEntity supplierByIdSupplier) { this.supplierByIdSupplier = supplierByIdSupplier; }
 
     @Override
     public boolean equals(Object o) {
